e6af3c9b6711481394d50509980cdb2a54f013fd
Viviana Pentangelo <pentangeloviviana@gmail.com>
2021-07-12 18:48:39 +0200

    bpy script + first commit

---
+++ blenderinvoker.py
@@ -0,0 +1,13 @@
+head = input("Head size: ")
+torso = input("Torso size: ")
+hips = input("Hips size: ")
+legs = input("Legs size: ")
+calves = input("Calves size: ")
+shoulders = input("Shoulders size: ")
+arms = input("Arms size: ")
+forearms = input("Forearms size: ")
+height = input("Height: ")
+
+
+
+print(height)
